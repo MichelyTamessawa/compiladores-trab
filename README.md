3a18ffba7ec3dc60e269cc0065376aa3a0068bd4
MichelyTamessawa <michelytamessawa@gmail.com>
2022-01-31 10:38:45 -0300

    upando o esqueleto scanner e parser

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# compiladores-trab
+# Trabalho de Compiladores
+
+Steps
+
+1. mkdir build
+2. cd build
+3. cmake ..
+4. make
---
+++ build/CMakeFiles/sc.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "parser.cc"
+  "parser.hh"
+  "scanner.cc"
+  "CMakeFiles/sc.dir/main.cc.o"
+  "CMakeFiles/sc.dir/driver.cc.o"
+  "CMakeFiles/sc.dir/scanner.cc.o"
+  "CMakeFiles/sc.dir/parser.cc.o"
+  "sc.pdb"
+  "../sc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
